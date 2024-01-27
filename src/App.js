eddb0c8caa8e37257bebe03b393bf129668a0a15
AntoineTheGreat0440E <poirierantoine52@gmail.com>
2024-01-27 15:03:48 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import TopicList from './mmf1_components/topic_list';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <TopicList/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/mmf1_components/topic_list.js
@@ -0,0 +1,10 @@
+import {React,useState} from 'react';
+import '../mmf1_styling/topic_list.css';
+
+function TopicList(){
+    return(<div className="topic_list">
+        <p>TOPIC LIST</p>
+    </div>);
+}
+
+export default TopicList;
